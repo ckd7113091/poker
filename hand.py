5c1032be31096820db154d351069a174c6fad2f2
Kevin George <kevingeorgetm@gmail.com>
2012-06-24 20:09:35 -0500

    Flushed out rankings, compares community cards.

--- hand.py
+++ hand.py
@@ -4,56 +4,75 @@ class Hand:
 	def __init__(self, cards):
 		self.hand = cards
 
+	def combine(self, cards):
+		combs = []
+		for i in range(7):
+			for j in range(i+1,7):
+				for k in range(j+1,7):
+					for l in range(j+1,7):
+						for m in range(l+1,7):
+							combs.append(sorted([cards[i],cards[j],\
+								cards[k],cards[l],cards[m]]))
+		return combs
+
+	def highest(self,community):
+		opts = self.combine(community+self.hand)
+		possible = [opts[i] for i in range(len(opts))]
+		return max(possible,key=self.rank)
+		#ranks = sorted([rank(self.hand + opts[i]) for i in len(opts)])
+
 	def rank(self,hand):
-		if straight(hand) and flush(hand):
+		if self.straight(hand) and self.flush(hand):
 			return [10,max(hand)]
-		if kind(4,hand):
-			return [9,kind(4,hand)]
-		if kind(3,hand) and kind(2,hand):
-			return [8, kind(3,hand),kind(2,hand)]
-		if flush(hand):
+		if self.kind(4,hand):
+			return [9,self.kind(4,hand)]
+		if self.kind(3,hand) and self.kind(2,hand):
+			return [8, self.kind(3,hand),self.kind(2,hand)]
+		if self.flush(hand):
 			return [7,hand]
-		if straight(hand):
+		if self.straight(hand):
 			return [6, max(hand)]
-		if kind(3,hand):
-			return [5, kind(3,hand)]
-		if len(kind(2,hand)) == 3:
-			return [4, kind(2,hand)]
-		if kind(2,hand):
-			return [3, kind(2,hand)]
+		if self.kind(3,hand):
+			return [5, self.kind(3,hand)]
+		if self.kind(2,hand) and len(self.kind(2,hand)) == 3:
+			return [4, self.kind(2,hand)]
+		if self.kind(2,hand):
+			return [3, self.kind(2,hand)]
 		else:
-			return max(hand)
+			return [2,max(hand)]
 
 	def straight(self,hand):
 		vals = [i[0] for i in hand]
-		for i in range(3):
+		for i in range(4):
 			if vals[i] + 1 != vals[i+1]:
-				return false
-		return true
+				return False
+		return True
 
 	def flush(self,hand):
 		suits = [i[1] for i in hand]
 		for suit in suits:
 			if suit != suits[0]:
-				return false
-		return true
+				return False
+		return True
 
 	def kind(self,of,hand):
 		vals = [i[0] for i in hand]
 		toret = []
-		inc = [False]*5
-		for i in range(5-of-1):
+		inc = [True]*5
+		for i in range(5-of):
 			start = True
 			for j in range(i+1,i+of):
 				if vals[j] != vals[i]:
 					start = False
 					break
 
+			if vals[i] == vals[i-1]: start = False
+			elif i+of < 3 and vals[i] != vals[i+of+1]: start = False
+
 			if start:
 				toret.append(vals[i])
-				inc[i:i+of] = [True]*of
+				inc[i:i+of] = [False]*of
 
-		a = [vals[j] for j in range(of)]
+			if len(toret) == 0: return
 
-		return [a for a in toret]
-#, [vals[j] for j in range(of) if not inc[j]]]
+		return [a for a in toret] + [[vals[j] for j in range(len(vals)) if inc[j]]]
